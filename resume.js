639fa46b324886e79a058492934166b30c3d85d4
Nikilraja <115686502+Nikilraja@users.noreply.github.com>
2022-10-28 21:27:13 +0530

    Add files via upload

---
+++ resume.js
@@ -0,0 +1,15 @@
+var resume=
+[{
+    "name":"nikil",
+    "age":"25"},
+
+    {"father name":"sivasankar",
+    "education":"bca"},
+
+    {"city":"villupuram",
+    "contact no":"8072595170"},
+    
+    {"gender":"male",
+    "nationality":"indian"
+}]
+console.log(resume);
---
+++ script.js
@@ -0,0 +1,44 @@
+var resume=
+[{
+    "name":"nikil",
+    "age":"25"},
+
+    {"father name":"sivasankar",
+    "education":"bca"},
+
+    {"city":"villupuram",
+    "contact no":"8072595170"},
+    
+    {"gender":"male",
+    "nationality":"indian"
+}]
+
+//for
+for(var i=0; i<resume.length; i++){
+    console.log(resume[i]);
+}
+
+//for in
+for(var key in resume){
+ console.log(resume[key]);
+}
+
+//for of
+for(var idex of resume){
+    console.log(idex);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
